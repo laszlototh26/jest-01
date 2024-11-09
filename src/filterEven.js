d6b8633fdc401c213de01b482fb6cd6e020cbb7e
laszlototh26 <toth1026@gmail.com>
2024-11-09 10:57:47 +0100

    implemented firstEven initial

---
+++ src/filterEven.js
@@ -0,0 +1,30 @@
+
+function filterEven (numbers) {
+
+  if ( !Array.isArray(number)) throw new Error ('Argument must be an array');
+
+  
+  const evenNumbers = numbers.filter ((item, index) => {
+
+
+
+    
+  }
+
+    const [, remainder] = divideNums(item, 2);
+
+
+
+
+  
+  
+  
+  )
+  
+  
+  
+
+}
+  module.exports = {
+    filterEven
+  }
